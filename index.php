930059f52275949236208f6cd24615c1808adcbf
Maria Felix <168776360+DudaF3lix@users.noreply.github.com>
2026-03-30 08:56:32 -0300

    atualização 30/03

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include("conexao.php");
+$sql = "SELECT * FROM produtos ORDER BY id Desc";
+$resultado = mysqli_query($conexao, $sql);
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SYS BRAIA</title>
+    </head>
+    <body>
+        <h1>Cadastro</h1>
+         <form action="salvar.php" method="post">
+            <label>Nome:</label>
+            <input type ="text" name="nome" required>
+            <label>Preço:</label>
+            <input type ="number" name="preco" required>
+            <label>Categoria:</label>
+            <input type ="text" name = "categoria" required>
+            <label>Quantidade:</label>
+            <input type = "number" name="quantidade">
+            <button type = "submit">Salvar</button>
+        </form>
+    </body>
+</html>
